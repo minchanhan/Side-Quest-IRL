ed4111bef70e479515e231b53c128414920e05c6
Minchan <minchanhan5@gmail.com>
2023-08-17 23:28:42 -0400

    added gitignore file

---
+++ Side Quest IRL/Side_Quest_IRLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Side_Quest_IRLApp.swift
+//  Side Quest IRL
+//
+//  Created by Minchan Han on 2023-08-17.
+//
+
+import SwiftUI
+
+@main
+struct Side_Quest_IRLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
